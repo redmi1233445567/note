89d286a3ff8a9ca50477d3fc17d2986e46cd9d00
redmi <hmadaredmi7@gmail.com>
2024-07-09 21:08:19 +0300

    add all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
+import React, { useState } from 'react'
+import Card from './component/Card';
 
 function App() {
+  const [data, setData] = useState({id: 0, advice: "go a head"});
+
+  const getData = async ()=> {
+    const res = await axios.get("https://api.adviceslip.com/advice");
+    setData(res.data.slip);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card data={data} getData={getData}/>
     </div>
   );
 }
---
+++ src/component/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Card({data, getData}) {
+    
+    const geterData = () => {
+        getData()
+    }
+    console.log(data)
+
+  return (
+    <div className='card'>
+      <div className='first'>
+        <span>ADVICE</span>
+        <span># {data.id}</span>
+      </div>
+      <h3>“{data.advice}”</h3>
+      <div className='sav'><svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg></div>
+      <div className='last' onClick={geterData}><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg></div>
+    </div>
+  )
+}
